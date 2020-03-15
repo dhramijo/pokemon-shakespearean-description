c64e598aa5982ebbd170e8670c385208fe0ff2be
dhramijo <dhrami86@yahoo.it>
2020-03-15 03:38:31 +0000

    Test Cases
    Updating Test cases missed in the first commit

--- src/test/java/com/truelayer/jonad/dhrami/PokemonShakespeareanDescriptionApplicationTests.java
+++ src/test/java/com/truelayer/jonad/dhrami/PokemonShakespeareanDescriptionApplicationTests.java
@@ -1,15 +1,15 @@
 package com.truelayer.jonad.dhrami;
 
-import static org.junit.Assert.assertNotNull;
-
 import com.truelayer.jonad.dhrami.controllers.PokemonController;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+import static org.junit.Assert.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class PokemonShakespeareanDescriptionApplicationTests {
 
